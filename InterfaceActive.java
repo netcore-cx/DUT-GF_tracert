598a50ef985377fff7254e99dda71991c9d6321d
ClementGiaime <clement.giaime@etu.unice.fr>
2017-02-16 21:44:21 +0100

    Branche InterfaceActive

---
+++ InterfaceActive.java
@@ -0,0 +1,33 @@
+public class InterfaceActive extends InterfaceMac{
+	public static final String IP="xxx.xxx.xxx.xxx";
+	private String ip;
+  private String mask;
+
+  //Constructeur
+	public InterfaceActive(){
+    super();
+		this.ip=IP;
+    this.mask=IP;
+	}
+	public  InterfaceActive(String macAdr, String ipAdr, String maskAdr){
+    super(macAdr);
+    this.ip=ipAdr;
+    this.mask=maskAdr;
+	}
+
+  //mutateur
+	public void setIp(String newIp){
+		this.ip=newIp;
+	}
+  public void setMask(String newMask){
+    this.mask=newMask;
+  }
+
+  //Accesseur en lecture
+  public Strinf getIp(){
+    return ip;
+  }
+	public String getMask(){
+		return mask;
+	}
+}
--- InterfaceMac.java
+++ InterfaceMac.java
@@ -2,24 +2,24 @@
 public class InterfaceMac {
 	public static final String MAC="0000.0000.0000";
 	private String mac;
-	
+
 	public InterfaceMac(){
 		this.mac=MAC;
 	}
 	public  InterfaceMac(String newMac){
 		this.mac=newMac;
 	}
-	
+
 	public void setMac(String newMac){
 		this.mac=newMac;
 	}
 	public String getMac(){
 		return mac;
 	}
-	
+
 	public static void main(String[] args){
 		InterfaceMac test=new InterfaceMac("14a5.5afb.2c2b");
 		System.out.println(test.getMac());
-		
+
 	}
 }
