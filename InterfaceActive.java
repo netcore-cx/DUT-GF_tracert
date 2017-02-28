00b46b020c780ddc563a611e0f0d61e0e6f8b8f3
LeiRoF <vince.lrf@gmail.com>
2017-02-28 12:11:16 +0100

    Methode toString
    Ajout de la méthode toString() retournant @mac:@ip:@mask

--- InterfaceActive.java
+++ InterfaceActive.java
@@ -1,35 +1,41 @@
 public class InterfaceActive extends InterfaceMac{
 	public static final String IP="xxx.xxx.xxx.xxx";
 	private String ip;
-  private String mask;
+ 	private String mask;
 
-  //Constructeur
+//Constructeurs
 	public InterfaceActive(){
-    super();
+    		super();
 		this.ip=IP;
-    this.mask=IP;
-	}
-	public  InterfaceActive(String macAdr, String ipAdr, String maskAdr){
-    super(macAdr);
-    this.ip=ipAdr;
-    this.mask=maskAdr;
+  		this.mask=IP;
 	}
 	
-	//mutateur
+	public  InterfaceActive(String macAdr, String ipAdr, String maskAdr){
+   		super(macAdr);
+  		this.ip = ipAdr;
+    		this.mask = maskAdr;
+	}
+
+//Accesseurs en lecture
+	public String toString(){
+		return super.toString() + ":" + this.IP + ":" + this.mask;
+	}
+	
+	public String getIp(){
+		return this.ip;
+	}
+	
+	public String getMask(){
+		return this.mask;
+	}
+	
+//Accesseurs en ecriture
 	public void setIp(String newIp){
 		this.ip=newIp;
 	}
 	
 	public void setMask(String newMask){
 		this.mask=newMask;
-	}
-	//Accesseur en lecture
-	public String getIp(){
-		return ip;
-	}
-	//Test git avec eclipse
+	}	
 	
-	public String getMask(){
-		return mask;
-	}
 }
