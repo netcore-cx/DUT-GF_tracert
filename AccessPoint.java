d1c0e19e366a490317d3292396c7f283e4ec6c93
LeiRoF <vince.lrf@gmail.com>
2017-02-28 13:38:38 +0100

    Accesspoint fin de la création de la class

--- AccessPoint.java
+++ AccessPoint.java
@@ -1,9 +1,70 @@
 import java.util.ArrayList;
 
-public class AccessPoint{
+public class AccessPoint extends Machine{
   public static final String DEFAULT_SSID = "None";
   public static final String DEFAULT_KEY = "None";
   private String ssid;
   private String key;
-  private ArrayList interfaces;
+  private ArrayList interfacesMac;
+  
+//Constructeurs
+  public AccessPoint(){
+    super();
+    ArrayList interfaces = new ArrayList();
+    this.ssid = DEFAULT_SSID;
+    this.key = DEFAULT_KEY;
+  }
+  
+  public AccessPoint(String newName, String newSsid){
+    super(newName);
+    ArrayList interfaces = new ArrayList();
+    this.ssid = newSsid;
+    this.key = DEFAULT_KEY;
+  }
+  
+  public AccessPoint(String newName, String newSsid, String newKey){
+    super(newName);
+    ArrayList interfaces = new ArrayList();
+    this.ssid = newSsid;
+    this.key = newKey;
+  }
+ 
+//Accesseurs en lecture
+  public String getSsid(){
+    return this.ssid;
+  }
+  
+  public String getKey(){
+    return this.key;
+  }
+  
+  public interfaceMac getInterfaceById(int Id){
+    return interfaces.get(Id);
+  }
+  
+//Accesseurs en écriture
+  public void setSsid(String newSsid){
+    this.ssid = newSsid;
+  }
+  
+  public void setKey(String newKey){
+    this.ssid = newKey;
+  }
+  
+  public interfaceMac addInterface(interfaceMac newInterface){
+    return interfaces.add(newInterface);
+  }
+  
+  public interfaceMac setInterfaceById(int id, interfaceMac newInterface){
+    return interfaces.set(id, newInterface);
+  }
+  
+  public interfaceMac delInterfaceById(int id){
+    return interfaces.remove(id);
+  }
+  
+//toString
+  public String toString(){
+    return super.toString() + ":" + this.ssid + ":" + this.key;
+  }
 }
