8e6a7325ef4525b2c6870d909f9aa0b1d6f5cf4a
LeiRoF <vince.lrf@gmail.com>
2017-02-28 12:26:14 +0100

    Creation class Machine
    Class contenant uniquement un nom (celui de la machine)

--- Machine.java
+++ Machine.java
@@ -1,32 +1,26 @@
 public class Machine {
-	public static final String MAC="0000.0000.0000";
-	private String mac;
+	public static final String DEFAULT_NAME = "Machine";
+	private String name;
 
 //Constructeurs
-	public InterfaceMac(){
-		this.mac=MAC;
+	public Machine(){
+		this.name = DEFAULT_NAME;
 	}
-	public  InterfaceMac(String newMac){
-		this.mac=newMac;
+	public  Machine(String newName){
+		this.name = newName;
 	}
 	
 //Accesseur en lecture
 	public String toString(){
-		return this.mac;
+		return this.name;
 	}
 	
-	public String getMac(){
-		return mac;
+	public String getName(){
+		return this.name;
 	}
 	
 //Accesseurs en ecriture
-	public void setMac(String newMac){
-		this.mac=newMac;
-	}
-	
-//Test git avec eclipse
-	public static void main(String[] args){
-		InterfaceMac test=new InterfaceMac("14a5.5afb.2c2b");
-		System.out.println(test.getMac());
+	public void setName(String newName){
+		this.name = newName;
 	}
 }
