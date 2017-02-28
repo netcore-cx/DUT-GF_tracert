a7fcdfb9d841bc124c7adc885e3da2ec51c7487f
LeiRoF <vince.lrf@gmail.com>
2017-02-28 12:57:12 +0100

    Creation de la class AccessPoint (non terminé)

---
+++ AccessPoint.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class AccessPoint{
+  public static final String DEFAULT_SSID = "None";
+  public static final String DEFAULT_KEY = "None";
+  private String ssid;
+  private String key;
+  private ArrayList interfaces;
+}
