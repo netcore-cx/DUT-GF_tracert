bc7311d2b02eef8ffd736c8b2401c19ff690bb98
ClementGiaime <ClementGiaime>
2017-03-02 15:23:59 +0100

    Ajoute des Mutateur et Accesseur (Switch, Router, AP)
    setInterfaceList
    addInterface
    setInterfaceById
    
    getInterfaceList
    getInterfaceById

--- class/test.java
+++ class/test.java
@@ -8,20 +8,25 @@ public class test{
   public static void main(String[] args){
 	  
 	  InterfaceOrdi temp;
+	  ArrayList<InterfaceOrdi> temp2;
+	  temp2 = new ArrayList<InterfaceOrdi>();
 
 	  Ordi pc = new Ordi("PcLinux");
 	  Ordi pc2 = new Ordi("PC_2_Linux");
 	  
 	  temp = new InterfaceOrdi("ad4e.4a5f.c9af", "192.168.1.1", "255.255.255.0", "192.168.1.254","8.8.8.8");
-	  pc.setAddInt(temp);
+	  temp2.add(temp);
+	  pc.setInterfaceList(temp2);
+	  
+	  
 	  temp = new InterfaceOrdi("a847.485a.caaf", "192.168.1.2", "255.255.255.0", "192.168.1.254","8.8.8.8");
-	  pc2.setAddInt(temp);
+	  pc2.addInterface(temp);
 	  
 	  System.out.println(pc.toString());
-	  System.out.println(pc.getInt().get(0).toString());
+	  System.out.println(pc.getInterfaceList().get(0).toString());
 	  
 	  System.out.println(pc2.toString());
-	  System.out.println(pc2.getInt().get(0).toString());
+	  System.out.println(pc2.getInterfaceById(0).toString());
 	  
 	  System.out.println("C'est qui le papa ??");
 	  }
