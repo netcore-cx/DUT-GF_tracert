34bb3675660a228d86db567cb48642044338dea9
LeiRoF <vince.lrf@gmail.com>
2017-03-10 15:47:24 +0100

    Update InterfaceMac.java
    Ajout de l'attribut machine créant un lien Interface -> Machine

--- class/InterfaceMac.java
+++ class/InterfaceMac.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class InterfaceMac {
 	public static final String MAC="0000.0000.0000";
 	static ArrayList<InterfaceMac> allInterfaces = new ArrayList<InterfaceMac>();
+	private Machine machine;
 	private String mac;
 	
 	//*******************
@@ -10,18 +11,28 @@ public class InterfaceMac {
   //*******************
 	public InterfaceMac(){
 		this.mac = MAC;
+		this.machine = new Machine();
 		allInterfaces.add(this);
 	}
 	public InterfaceMac(String newMac){
 		this.mac = newMac;
 		allInterfaces.add(this);
 	}
+	public InterfaceMac(String newMac, Machine machine){
+		this.mac = newMac;
+		this.machine = machine;
+		allInterfaces.add(this);
+	}
+	
 
 	//********************
   //***** Mutateur *****
   //********************
 	public void setMac(String newMac){
 		this.mac=newMac;
+	}
+	public void setMachine(Machine machine){
+		this.machine = machine;
 	}
 
 	//*******************
@@ -30,6 +41,9 @@ public class InterfaceMac {
 	public String getMac(){
 		return mac;
 	}
+	public Machine getMachine(){
+		return machine;
+	}
 	
 	public ArrayList<InterfaceMac> getIntMacList(){
 		return allInterfaces;
@@ -37,6 +51,6 @@ public class InterfaceMac {
 
 //toString
 	public String toString(){
-		return mac;
+		return machine.toString() + "|" + mac;
 	}
 }
